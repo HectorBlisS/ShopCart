b3a06051c75eaef192e0ab4155a9f21a56bbb746
HectorBlisS <contacto@fixter.org>
2016-09-05 10:45:31 -0500

    carrito done

--- tiendita/settings.py
+++ tiendita/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'tiendita.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -65,6 +65,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                #Mi carrito de compras
+                'cart.context_processors.cart',
             ],
         },
     },
